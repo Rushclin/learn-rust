7d6c2659a6cbc241688a28cf8c48931993c5b418
Rushclin <takamrushclin@gmail.com>
2026-03-12 16:09:29 +0100

    manage struct - 1

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,41 @@
-use std::fmt;
-
-fn reverse(pair: (i32, String)) -> (String, i32) {
-    let (int_params, str_params) = pair;
-
-    return (str_params, int_params);
+struct Person{
+    name: String,
+    age: u8,
+}
+#[derive(Debug)]
+struct Point{
+    x: f64,
+    y: f64,
 }
 
-fn matrix_reverse(matrix: Matrix) -> Matrix{
-    let Matrix(a, b, c, d) = matrix;
-    Matrix(d, c, b, a)
+#[derive(Debug)]
+struct Rectangle{
+    top_left: Point,
+    bottom_right: Point,
 }
 
-fn matrix_transpose(matrix: Matrix) -> Matrix {
-    let Matrix(a, b, c, d) = matrix;
-    Matrix(a, c, b, d)
-}
-#[derive(Debug, Clone, PartialEq, Eq, Copy)]
-struct Matrix(i32, i32, i32, i32);
-
-impl fmt::Display for Matrix {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {}, {}, {})", self.0, self.1, self.2, self.3)
-    }   
+fn calculate_area(rectangle: &Rectangle) -> f64 {
+    let width = rectangle.bottom_right.x - rectangle.top_left.x;
+    let height = rectangle.top_left.y - rectangle.bottom_right.y;
+    width * height
 }
 
-fn main() {
-    let long_tule = (10, String::from("Takam"), true);
-    let matrix = Matrix(1, 2, 3, 4);
+fn main(){
+
+    let name = String::from("Alice");
+    let age = 30;
+    let person = Person { name, age };
+    let point_x = Point { x: 1.0, y: 2.0 };
+    let point_y = Point { x: 3.0, y: 4.0 };
+    let bottom_right = Point { x: 5.0, ..point_y };
+    println!("Point X: ({}, {})", point_x.x, point_x.y);
+    println!("Point Y: ({}, {})", point_y.x, point_y.y);
+    println!("Bottom Right: ({}, {})", bottom_right.x, bottom_right.y);
+    let rectangle = Rectangle { top_left: point_x, bottom_right: bottom_right };
+    println!("Person: {}", person.name);
+    println!("Rectangle: {:?}", rectangle);
+
+    println!("Area of the rectangle: {}", calculate_area(&rectangle));
 
     println!("Hello, world!");
-    println!(
-        "I want to reverse (10, 'Takam') => {:?}",
-        reverse((10, String::from("Takam")))
-    );
-    println!("Long tuple: {:?}", long_tule);
-    println!("Ma Matrix: {}", matrix);
-    println!("Ma Matrix transpose: {}", matrix_transpose(matrix));
-    println!("Ma Matrix reverse: {}", matrix_reverse(matrix));
 }
